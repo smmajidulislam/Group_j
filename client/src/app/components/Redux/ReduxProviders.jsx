00d95b6202e730027a976a6e6e0b6b0d16662aac
S M Majidul Islam <smmajidulislamdumuriakhulna876@gmail.com>
2025-05-04 00:34:36 +0600

    agin bug Fixed

--- client/src/app/components/Redux/ReduxProviders.jsx
+++ client/src/app/components/Redux/ReduxProviders.jsx
@@ -1,5 +1,5 @@
 "use client";
-import store from "@/app/apps/store";
+import store from "@/app/store/store";
 import { Provider } from "react-redux";
 const ReduxProviders = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
