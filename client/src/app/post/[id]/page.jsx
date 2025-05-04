5bd16770e10514c7a8c78cdb6dbd1e9a3fd2a296
S. M. MAJIDUL ISLAM <smmajidulislam77@gmail.com>
2025-05-04 08:12:35 +0600

    Update page.jsx singel post id fixed
    Update page.jsx singel post id fixed

--- client/src/app/post/[id]/page.jsx
+++ client/src/app/post/[id]/page.jsx
@@ -2,8 +2,8 @@
 
 import Singelpost from "@/app/components/Singelpost";
 
-const Page = ({ params }) => {
-  const id = params?.id[0];
+const Page = async({ params }) => {
+  const id = await params?.id;
 
   return (
     <div className="max-w-4xl mx-auto px-4 py-6 text-white">
